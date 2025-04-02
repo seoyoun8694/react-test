9dbb4f28906f3e640cc5a23e3d8658d7487c5e8c
seoyun <chaeseoyoun8694@gmail.com>
2025-04-02 14:03:36 +0900

    04.02

--- react-sample/src/Footer.js
+++ react-sample/src/Footer.js
@@ -1,13 +1,12 @@
 function Footer(props) {
-    return (
-        <footer>
-            {
-                props.footers.map((v, i) => {
-                    return <h5 key={i}>{v}</h5>
-                })
-            }
-        </footer>
-    );
+  return (
+    <footer>
+      {
+        props.footers.map((v, i) => {
+          return <h5 key={i}>{v}</h5>
+        })
+      }
+    </footer>
+  );
 }
-
 export default Footer;
