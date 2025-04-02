f0ae6ef1d080bbacbf3b4ecc3a37aaf2e25fa01c
seoyun <chaeseoyoun8694@gmail.com>
2025-04-02 14:03:36 +0900

    04.02
    4교시 - sample

--- react-sample/src/Header.js
+++ react-sample/src/Header.js
@@ -1,9 +1,10 @@
 function Header() {
-    return (
-        <header>
-            <h1>야구게임 방식</h1>
-        </header>
-    );
+  return (
+    <header>
+      <h1 style={{
+        backgroundColor: "red"
+      }}>야구게임 방식</h1>
+    </header>
+  );
 }
-
 export default Header;
--- react-sample/src/Section.js
+++ react-sample/src/Section.js
@@ -1,13 +1,12 @@
 function Section(props) {
-    return (
-        <section>
-            {
-                props.sections.map((v, i) => {
-                    return <h4 key={i}>{v}</h4>
-                })
-            }
-        </section>
-    );
+  return (
+    <section>
+      {
+        props.sections.map((v, i) => {
+          return <h4 key={i}>{v}</h4>
+        })
+      }
+    </section>
+  );
 }
-
 export default Section;
