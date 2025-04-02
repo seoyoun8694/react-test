0344a203530e65787f7939db1fd0881f472b490f
seoyun <chaeseoyoun8694@gmail.com>
2025-04-02 15:15:54 +0900

    04.02

--- react-cra/src/Header.js
+++ react-cra/src/Header.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 function Header(props) {
     console.log('header');
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('처음에만 실행!');
+    }, []);
+
+    useEffect(() => {
+        console.log('count가 바뀔때 실행!');
+    }, [count]);
+
+    function click(e) {
+        alert(e.target.innerText);
+    }
+
     return (
         <header>
-            <h1>{props.title}</h1>
-            {props.desc};
+            <h1 onClick={click}>{props.title}</h1>
+            <p onClick={() => {
+                //setCount(prev => prev + 1);
+                setCount(count + 1);
+            }}>{props.desc} {count}</p>
         </header>
     );
 }
